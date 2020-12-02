581c791017e22a446d107be612c58d686a1ccefb
sonx <sonxz@qq.com>
2020-12-02 17:10:34 +0800

    perf: 穷举算法
    <body>
    
    <footer>

--- tmp.go
+++ tmp.go
@@ -13,13 +13,13 @@ import (
 var target = 770
 var m = make(map[string]int)
 
-//var p = []int{731, 121, 520, 214, 145, 336, 327, 372, 411, 613}
+// var p = []int{731, 121, 520, 214, 145, 336, 327, 372, 411, 613}
 //var p = []int{731, 121, 520, 214, 145, 336, 327, 613, 260}
 // var p = []int{731, 121, 214, 145, 336, 327, 613}
 var p = []int{731, 214, 145, 613}
 
 func main() {
-	arr := subsets(p)
+	arr := mysubsets(p)
 
 	r := make([]int, 0)
 	for _, v := range arr {
@@ -40,27 +40,25 @@ func main() {
 			}
 		}
 	}
-	//fmt.Println(arr)
-	fmt.Println(r)
+	fmt.Printf("最佳组合：%v \n", r)
 }
 
-func subsets(nums []int) [][]int {
-	arr := make([][]int, 0)
-	arr = append(arr, []int{})
-	for i := 0; i < len(nums); i++ {
-		tempArr := make([][]int, 0)
-		for _, c := range arr {
-			temp := make([]int, 0)
-			temp = append(temp, c...)
-			temp = append(temp, nums[i])
-			//fmt.Println(temp)
-			tempArr = append(tempArr, temp)
-		}
-		for _, c := range tempArr {
-			arr = append(arr, c)
+// 用二维数组（result）存储所有的组合，每一个元素（数组[]int）就是一个组合
+// 思路：遍历二维数组，对每一个元素（数组[]int）加上遍历nums数组的一个元素，这样nums中的元素不会出现自我重叠重复的现象。
+// 并且result中的每个组合都会加上新的num后会作为新的[]int元素添加到result中
+// 把遍历后并加上新的nums元素的新元素（数组[]int），再添加到二维数组中作为新组合。
+func mysubsets(nums []int) [][]int {
+	result := make([][]int, 0)
+	result = append(result, []int{})
+	// 每一个nums元素都往result里的组合都添加一遍
+	for _, n := range nums {
+		for _, arr := range result {
+			arr = append(arr, n)
+			result = append(result, arr)
 		}
 	}
-	return arr[1:]
+	fmt.Printf("总共组合：%d 个\n", len(result))
+	return result
 }
 
 //计算slice中各项之和
