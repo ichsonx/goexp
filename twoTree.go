e2903dd6255a0adc5ac89c7c655b3e706dfd3db8
sonx <sonxz@qq.com>
2020-12-09 17:14:33 +0800

    perf: update
    <body>
    
    <footer>

--- twoTree.go
+++ twoTree.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -12,9 +13,59 @@ type TreeNode struct {
 }
 
 func (t TreeNode) display() {
-	fmt.Printf("节点值：%d/n", t.Val)
+	fmt.Printf("节点值：%d\n", t.Val)
+}
+
+func (t *TreeNode) AddNode(val int) error {
+	if t.Left != nil && t.Right != nil {
+		return errors.New(fmt.Sprintf("error：当前节点度数为2（已满），不能再添加子节点\n"))
+	}
+
+	newNode := TreeNode{Val: val}
+	if t.Left == nil {
+		t.Left = &newNode
+	} else {
+		t.Right = &newNode
+	}
+	return nil
+}
+func (t *TreeNode) AddLeftNode(val int) error {
+	if t.Left != nil {
+		return errors.New(fmt.Sprintf("error：左子节点不为空\n"))
+	} else {
+		newNode := TreeNode{Val: val}
+		t.Left = &newNode
+	}
+	return nil
+}
+func (t *TreeNode) AddRightNode(val int) error {
+	if t.Right != nil {
+		return errors.New(fmt.Sprintf("error：右子节点不为空\n"))
+	} else {
+		newNode := TreeNode{Val: val}
+		t.Right = &newNode
+	}
+	return nil
+}
+
+func (t TreeNode) duDisplay() {
+	du := 0
+	if t.Right != nil {
+		du = 2
+	} else if t.Left != nil {
+		du = 1
+	}
+	fmt.Printf("度数：%d\n", du)
 }
 
 func main() {
 	fmt.Println("Hello World!")
+	t := TreeNode{Val: 1}
+	t.AddNode(5)
+	err := t.AddNode(9)
+	if err != nil {
+		fmt.Print(err)
+	}
+	t.display()
+	t.duDisplay()
 }
