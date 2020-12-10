239756ed0ac9cc4ec63904e75a7a8cdce59e1951
sonx <sonxz@qq.com>
2020-12-10 16:40:02 +0800

    perf: update
    <body>
    
    <footer>

--- twoTree.go
+++ twoTree.go
@@ -6,29 +6,32 @@ import (
 	"fmt"
 )
 
+// 二叉树结构
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+// 显示当前节点的值
 func (t TreeNode) display() {
 	fmt.Printf("节点值：%d\n", t.Val)
 }
 
-func (t *TreeNode) AddNode(val int) error {
+// 自动添加节点，顺序从左到右。返回error
+func (t *TreeNode) AddNode(val int) (err error) {
 	if t.Left != nil && t.Right != nil {
 		return errors.New(fmt.Sprintf("error：当前节点度数为2（已满），不能再添加子节点\n"))
 	}
 
-	newNode := TreeNode{Val: val}
-	if t.Left == nil {
-		t.Left = &newNode
-	} else {
-		t.Right = &newNode
+	if err = t.AddLeftNode(val); err != nil {
+		err = t.AddRightNode(val)
 	}
-	return nil
+
+	return err
 }
+
+// 添加左节点，返回error
 func (t *TreeNode) AddLeftNode(val int) error {
 	if t.Left != nil {
 		return errors.New(fmt.Sprintf("error：左子节点不为空\n"))
@@ -38,6 +41,8 @@ func (t *TreeNode) AddLeftNode(val int) error {
 	}
 	return nil
 }
+
+// 添加右节点，返回error
 func (t *TreeNode) AddRightNode(val int) error {
 	if t.Right != nil {
 		return errors.New(fmt.Sprintf("error：右子节点不为空\n"))
@@ -48,14 +53,15 @@ func (t *TreeNode) AddRightNode(val int) error {
 	return nil
 }
 
-func (t TreeNode) duDisplay() {
+// 返回当前节点的度
+func (t TreeNode) Du() int {
 	du := 0
 	if t.Right != nil {
 		du = 2
 	} else if t.Left != nil {
 		du = 1
 	}
-	fmt.Printf("度数：%d\n", du)
+	return du
 }
 
 func main() {
@@ -67,5 +73,4 @@ func main() {
 		fmt.Print(err)
 	}
 	t.display()
-	t.duDisplay()
 }
